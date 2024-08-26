3f5b7498a13229a9feb340d536ed83eb75426391
norielagartosacristan <norielagartosacristan@gmail.com>
2024-08-26 18:56:13 +0800

    Update footer.php
    Added text to the file.

---
+++ footer.php
@@ -0,0 +1,27 @@
+
+
+<script>
+    $(document).ready(function() {
+        $('#myTable').DataTable();
+    });
+</script>
+
+
+
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootsrap5.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script> src="js/main.js"</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.boostrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdn.datatables.net/2.1.4/js/dataTables.js"></script>
+
+<script src="java/main.js"></script> 
+
+</body>
+</html>
